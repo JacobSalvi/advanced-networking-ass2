26526f0c5da3977c66f20d6dc9f2333abd5a3095
JacobSalvi <43408191+JacobSalvi@users.noreply.github.com>
2024-04-13 14:31:49 +0200

    test.

--- src/emulation_simple.py
+++ src/emulation_simple.py
@@ -257,8 +257,9 @@ class NetworkDefinition:
 
                 for si in source_node_interfaces:
                     complete_subnet_address = si.complete_address()
-                    print(f"{node_name} ip route add {si.address} via {link.address} dev {link.link_name}")
-                    # net[node_name].cmd(f"ip route add {complete_subnet_address} via {link.address}")
+                    routing_table_entry = f"{node_name} ip route add {si.address} via {link.address}"
+                    print(routing_table_entry)
+                    net[node_name].cmd(routing_table_entry)
                 pass
             #  router1.cmd('ip route add 10.0.2.0/24 via 10.1.2.2')
             pass
